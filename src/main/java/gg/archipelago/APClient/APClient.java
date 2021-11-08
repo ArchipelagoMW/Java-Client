1bbed1f0636e6322ed2bfc3bc6c67117d998f789
Kono Tyran <Kono.Tyran@gmail.com>
2021-11-07 16:13:51 -0800

    increment protocol version. update to compile against java16

--- src/main/java/gg/archipelago/APClient/APClient.java
+++ src/main/java/gg/archipelago/APClient/APClient.java
@@ -39,7 +39,7 @@ public abstract class APClient {
     private final ItemManager itemManager;
     private final DataManager dataManager;
 
-    public static final Version protocolVersion = new Version(0,1,6);
+    public static final Version protocolVersion = new Version(0,1,9);
 
     private int team;
     private int slot;
