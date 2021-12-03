ea342d2d9c1a39a804e64c482f6566bffee76ffc
Kono Tyran <HAklowner@gmail.com>
2021-12-02 19:56:34 -0800

    Version Bump

--- src/main/java/gg/archipelago/APClient/APClient.java
+++ src/main/java/gg/archipelago/APClient/APClient.java
@@ -40,7 +40,7 @@ public abstract class APClient {
     private final ItemManager itemManager;
     private final DataManager dataManager;
 
-    public static final Version protocolVersion = new Version(0,1,9);
+    public static final Version protocolVersion = new Version(0,2,1);
 
     private int team;
     private int slot;
