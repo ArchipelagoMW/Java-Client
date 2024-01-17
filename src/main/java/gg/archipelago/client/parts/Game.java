649e7a6367bb4228d4a8fb3b8a1ddfbe810e8b2c
Charlesfire <charlesfire2@hotmail.com>
2024-01-17 12:32:30 -0500

    Fixed locations missing from locationIdToName

--- src/main/java/gg/archipelago/client/parts/Game.java
+++ src/main/java/gg/archipelago/client/parts/Game.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName;
 import java.io.Serializable;
 import java.util.HashMap;
 
-public class Game implements Serializable {
+public class Game implements Serializable, Cloneable {
 
     @SerializedName("version")
     public int version;
@@ -18,4 +18,20 @@ public class Game implements Serializable {
 
     @SerializedName("location_name_to_id")
     public HashMap<String,Long> locationNameToId = new HashMap<>();
+
+    @Override
+    public Game clone() {
+        try {
+            Game clone = (Game) super.clone();
+
+            clone.version = this.version;
+            clone.hash = this.hash;
+            clone.itemNameToId = (HashMap<String, Long>) this.itemNameToId.clone();
+            clone.locationNameToId = (HashMap<String, Long>) this.locationNameToId.clone();
+
+            return clone;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
 }
