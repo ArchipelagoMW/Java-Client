7d947ac17008aa8ccfa0f005030ce8dd40bb1dd5
Charlesfire <charlesfire2@hotmail.com>
2024-01-17 17:15:40 -0500

    Removed unused method clone()

--- src/main/java/gg/archipelago/client/parts/Game.java
+++ src/main/java/gg/archipelago/client/parts/Game.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName;
 import java.io.Serializable;
 import java.util.HashMap;
 
-public class Game implements Serializable, Cloneable {
+public class Game implements Serializable {
 
     @SerializedName("version")
     public int version;
@@ -18,20 +18,4 @@ public class Game implements Serializable, Cloneable {
 
     @SerializedName("location_name_to_id")
     public HashMap<String,Long> locationNameToId = new HashMap<>();
-
-    @Override
-    public Game clone() {
-        try {
-            Game clone = (Game) super.clone();
-
-            clone.version = this.version;
-            clone.hash = this.hash;
-            clone.itemNameToId = (HashMap<String, Long>) this.itemNameToId.clone();
-            clone.locationNameToId = (HashMap<String, Long>) this.locationNameToId.clone();
-
-            return clone;
-        } catch (CloneNotSupportedException e) {
-            throw new AssertionError();
-        }
-    }
 }
