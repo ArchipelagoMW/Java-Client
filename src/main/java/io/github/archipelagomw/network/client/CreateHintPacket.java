17391181d697f053f558014205739272fdb90579
platanobailando <cjmang@gmail.com>
2025-09-03 21:09:07 -0500

    Removing spurious main

--- src/main/java/io/github/archipelagomw/network/client/CreateHintPacket.java
+++ src/main/java/io/github/archipelagomw/network/client/CreateHintPacket.java
@@ -1,14 +1,9 @@
 package io.github.archipelagomw.network.client;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import io.github.archipelagomw.network.APPacket;
 import io.github.archipelagomw.network.APPacketType;
-import io.github.archipelagomw.utils.IntEnum;
-import io.github.archipelagomw.utils.IntEnumAdapterFactory;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class CreateHintPacket extends APPacket {
@@ -26,15 +21,5 @@ public class CreateHintPacket extends APPacket {
         this.locations = locations;
         this.player = player;
         this.status = status;
-    }
-
-    public static void main(String[] args) {
-        Gson gson = new GsonBuilder().registerTypeAdapterFactory(new IntEnumAdapterFactory()).create();
-        CreateHintPacket packet = new CreateHintPacket(Arrays.asList(10L), 1, HintStatus.HINT_UNSPECIFIED);
-        String result;
-        System.out.println(result = gson.toJson(packet));
-        CreateHintPacket round = gson.fromJson(result, CreateHintPacket.class);
-        System.out.println(round.status);
-
     }
 }
